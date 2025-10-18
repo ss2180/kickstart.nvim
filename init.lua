29da4e4eb0ca5ab7948a6260ae53db362afee2aa
Solomon Chamberlin <solomon.chamberlin@hotmail.co.uk>
2025-10-18 23:00:41 +0100

    First changes to kickstart

--- init.lua
+++ init.lua
@@ -91,12 +91,17 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
+
+-- Set tabwidth and expand
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
 -- Make line numbers default
 vim.o.number = true
